23bede3babf2df85fc271ad7c8575610d7e09392
Andrej Kabachnik <kabachello@users.noreply.github.com>
2024-07-31 09:47:42 +0200

    DEV base URL working

--- Facades/SchemioFacade.php
+++ Facades/SchemioFacade.php
@@ -25,23 +25,35 @@ class SchemioFacade extends AbstractHttpFacade
         $path = $uri->getPath();
         $headers = $this->buildHeadersCommon();
         
+        $baseUrl = $this->getWorkbench()->getUrl();
+        $appUrl = $baseUrl . $this->getUrlRouteDefault();
+        
         // api/schemio/index.html/ -> index.html
         $pathInFacade = mb_strtolower(StringDataType::substringAfter($path, $this->getUrlRouteDefault() . '/'));
         
         // Do the routing here
         switch (true) {     
-            case $pathInFacade === null:
-            case $pathInFacade === '':
-            case $pathInFacade === 'index.html':
-                $filePath = $this->getFilePath('html' . DIRECTORY_SEPARATOR . 'index.html');
+            case StringDataType::endsWith($pathInFacade, 'schemio.app.js'):
+                $filePath = $filePath = $this->getFilePath($pathInFacade);
                 $body = file_get_contents($filePath);
-                $type = MimeTypeDataType::findMimeTypeOfFile($filePath);
-                $headers['Content-Type'] = $type;
+                $body = str_replace([
+                    'rootPath: "/",',
+                    'assetsPath: "/assets",',
+                    '"/v1/'
+                ], [
+                    'rootPath: "' . $appUrl . '",',
+                    'assetsPath: "' . $appUrl . '/assets",',
+                    '"v1/'
+                ], $body);
+                $headers['Content-Type'] = 'text/javascript';
                 $responseCode = 200;
                 break;
-            case StringDataType::endsWith($pathInFacade, '.html'):
+            case StringDataType::endsWith($pathInFacade, '.html') && $pathInFacade !== 'index.html':
             case StringDataType::endsWith($pathInFacade, '.css'):
             case StringDataType::endsWith($pathInFacade, '.js'):
+            case StringDataType::endsWith($pathInFacade, '.png'):
+            case StringDataType::endsWith($pathInFacade, '.woff2'):
+            case StringDataType::endsWith($pathInFacade, '.tff'):
                 $folder = StringDataType::endsWith($pathInFacade, '.html') ? 'html' : '';
                 $filePath = $this->getFilePath($folder . DIRECTORY_SEPARATOR . $pathInFacade);
                 if ($filePath !== null) {
@@ -52,12 +64,28 @@ class SchemioFacade extends AbstractHttpFacade
                 } else {
                     $responseCode = 404;
                 }
-                break;   
-            case StringDataType::startsWith($pathInFacade, 'adminer/'):
-            case StringDataType::startsWith($pathInFacade, 'externals/'):
-                return $this->runAdminer($pathInFacade);
+                break;
+            case StringDataType::startsWith($pathInFacade, 'v1/fs'):
+                $json = [
+                    "path" => "",
+                    "viewOnly" => false,
+                    "entries" => []
+                ];
+                $body = json_encode($json);
+                $headers['Content-Type'] = 'application/json';
+                $responseCode = 200;
+                break;
+            case $pathInFacade === null:
+            case $pathInFacade === '':
+            case $pathInFacade === 'index.html':
             default:
-                $responseCode = 404;
+                $filePath = $this->getFilePath('html' . DIRECTORY_SEPARATOR . 'index.html');
+                $body = file_get_contents($filePath);
+                $type = MimeTypeDataType::findMimeTypeOfFile($filePath);
+                $headers['Content-Type'] = $type;
+                $responseCode = 200;
+                break;
+                
         }
         
         return new Response(($responseCode ?? 404), $headers, stream_for($body ?? ''));
